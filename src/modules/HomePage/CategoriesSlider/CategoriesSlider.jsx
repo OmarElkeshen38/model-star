256e5c2705b16705336653e5820f0dd0560e57a4
OmarElkeshen38 <oelkeshen5@gmail.com>
2025-08-20 03:14:00 +0300

    display categories

--- src/modules/HomePage/CategoriesSlider/CategoriesSlider.jsx
+++ src/modules/HomePage/CategoriesSlider/CategoriesSlider.jsx
@@ -1,26 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import { useTranslation } from "react-i18next";
-
-import shoiceImg from '../../../assets/categories/menn.jpg';
-import mennImg from '../../../assets/categories/menn.jpg';
-import womenImg from '../../../assets/categories/women.jpg';
-import sunglasessImg from '../../../assets/categories/sunglasess.jpg';
-import watchsImg from '../../../assets/categories/watchs.png';
-import shoesImg from '../../../assets/categories/shoes.png';
+import { getCategories } from '../../Admin/AdminCategories/categorySlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 function CategoriesSlider() {
     const { t, i18n } = useTranslation();
+    const dispatch = useDispatch();
+    const { items: categories, loading } = useSelector((state) => state.categories);
 
-    const categories = [
-        { id: 1, name: t("categories.shoes"), image: shoesImg },
-        { id: 3, name: t("categories.watches"), image: watchsImg },
-        { id: 4, name: t("categories.sunglasses"), image: sunglasessImg },
-        { id: 5, name: t("categories.electronics"), image: shoiceImg },
-        { id: 6, name: t("categories.mens-clothing"), image: mennImg },
-        { id: 7, name: t("categories.womens-clothing"), image: womenImg },
-    ];
+    useEffect(() => {
+            dispatch(getCategories());
+        }, [dispatch]);
 
     return (
         <section className="py-20 bg-gradient-to-b from-indigo-50 via-white to-gray-100">
@@ -47,14 +39,14 @@ function CategoriesSlider() {
                     }}
                 >
                     {categories.map((cat) => (
-                        <SwiperSlide key={cat.id}>
+                        <SwiperSlide key={cat._id}>
                             <div className="bg-white cursor-pointer rounded-lg shadow-md hover:shadow-xl transition-transform duration-300 transform hover:scale-105 p-4 text-center animate-fade-in-up">
                                 <img
-                                    src={cat.image}
-                                    alt={cat.name}
+                                    src={cat.icon.secure_url}
+                                    alt={cat.name_ar}
                                     className="w-full h-56 object-cover rounded-md mb-4"
                                 />
-                                <h3 className="text-gray-800 font-semibold text-lg">{cat.name}</h3>
+                                <h3 className="text-gray-800 font-semibold text-lg">{cat.name_ar}</h3>
                             </div>
                         </SwiperSlide>
                     ))}
